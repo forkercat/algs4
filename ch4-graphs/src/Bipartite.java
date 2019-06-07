cb6ac61c75e928e1f2777184635c96aec276492a
Junhao Wang <junhaowww@outlook.com>
2019-06-07 11:11:14 -0700

    add UndirectedCycle & Bipartite

---
+++ ch4-graphs/src/Bipartite.java
@@ -0,0 +1,37 @@
+/**
+ * @author Junhao Wang
+ * @date 06/07/2019
+ */
+
+public class Bipartite {
+  private boolean[] marked;
+  private boolean[] color;
+  private boolean isBipartite = true; // true: U-V-U  false: U-V-V
+
+  public Bipartite(Graph G) {
+    marked = new boolean[G.V()];
+    color = new boolean[G.V()];
+    for (int s = 0; s < G.V(); s += 1) {
+      if (marked[s] == false) {
+        dfs(G, s);
+      }
+    }
+  }
+
+  public void dfs(Graph G, int v) {
+    marked[v] = true;
+
+    for (int w : G.adj(v)) {
+      if (marked[w] == false) {
+        color[w] = !color[v];
+        dfs(G, w);
+      } else if (color[v] == color[w]) { // find marked vertex
+        isBipartite = false;
+      }
+    }
+  }
+
+  public boolean isBipartite() {
+    return isBipartite;
+  }
+}
---
+++ ch4-graphs/src/UndirectedCycle.java
@@ -0,0 +1,34 @@
+/**
+ * @author Junhao Wang
+ * @date 06/07/2019
+ */
+
+public class UndirectedCycle {
+  private boolean[] marked;
+  private boolean hasCycle;
+
+  public UndirectedCycle(Graph G) {
+    marked = new boolean[G.V()];
+    for (int s = 0; s < G.V(); s += 1) {
+      if (marked[s] == false) {
+        dfs(G, -1, s);
+      }
+    }
+  }
+
+  // u is the previous node of v
+  public void dfs(Graph G, int u, int v) { // Note: This is only for undirected graph
+    marked[v] = true;
+    for (int w : G.adj(v)) {
+      if (marked[w] == false) {
+        dfs(G, v, w);  // v is not equal to u
+      } else if (w != u) { // u - v - w. If w == u, it is not connected but bidirectional.
+        hasCycle = true; // can I just return? maybe. but the marked is not completed
+      }
+    }
+  }
+
+  public boolean hasCycle() {
+    return hasCycle;
+  }
+}
